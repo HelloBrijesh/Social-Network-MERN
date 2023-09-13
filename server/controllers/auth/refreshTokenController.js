e18d82bea16fa5b4651d51e41adc7a5f0cd63c80
Brijeshkumar Patel <brijeshpatelcan@gmail.com>
2023-09-13 09:24:41 -0500

    Feat : Created Refresh token Controller

---
+++ server/controllers/auth/refreshTokenController.js
@@ -0,0 +1,67 @@
+import { JWT_REFRESH_SECRET } from "../../config";
+import { RefreshTokenModel, UserModel } from "../models";
+import { customErrorHandler, jwtService } from "../services";
+
+const refreshTokenController = {
+  async refreshToken() {
+    let cookieToken = req.headers.cookie;
+    if (!cookieToken) {
+      return next(customErrorHandler.unAuthorized("Invalid Refresh Token"));
+    }
+    const cookieRefreshtoken = cookieToken.split("=")[1];
+
+    try {
+      let verifiedRefreshToken = await RefreshTokenModel.findOne({
+        savedRefreshToken: cookieRefreshtoken,
+      });
+
+      if (!verifiedRefreshToken) {
+        return next(customErrorHandler.unAuthorised("Invalid Refresh Token"));
+      }
+    } catch (error) {
+      return next(error);
+    }
+
+    let userId;
+    try {
+      const { _id } = jwtService.verify(cookieRefreshtoken, JWT_REFRESH_SECRET);
+      userId = _id;
+    } catch (error) {
+      return next(customErrorHandler.unAuthorised("Invalid Refresh Token"));
+    }
+
+    // Finding the user from database
+    try {
+      const user = await UserModel.findOne({ _id: userId });
+      if (!user) return next(customErrorHandler.unAuthorized("No user found!"));
+    } catch (error) {
+      return next(error);
+    }
+
+    // Creating new Tokens
+    let access_token;
+    let refresh_token;
+    try {
+      access_token = jwtService.sign({ _id: user._id, role: user.role });
+      refresh_token = jwtService.sign(
+        { _id: user._id, firstName: user.firstName },
+        JWT_REFRESH_SECRET,
+        "7d"
+      );
+
+      // Adding new refresh_token in database
+      await RefreshTokenModel.create({ savedRefreshToken: refresh_token });
+
+      // Deleting the old refresh_token from database
+      await RefreshTokenModel.deleteOne({
+        savedRefreshToken: cookieRefreshtoken,
+      });
+    } catch (error) {
+      return next(new Error("Something went wrong " + error.message));
+    }
+
+    res.json({ access_token });
+  },
+};
+
+export default refreshTokenController;
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,3 +2,4 @@ export { default as signupController } from "./auth/signupController";
 export { default as loginController } from "./auth/loginController";
 export { default as emailVerificationController } from "./auth/emailVerificationController";
 export { default as changePasswordController } from "./auth/changePasswordController";
+export { default as refreshTokenController } from "./auth/refreshTokenController";
