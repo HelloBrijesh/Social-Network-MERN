8e15ccc76683749754044a1237b6cf91cbb52e18
Brijeshkumar Patel <brijeshpatelcan@gmail.com>
2023-09-13 09:04:39 -0500

    fix : fix the typing errors

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,24 +1,24 @@
-import { CustomErrorHandler, JwtService } from "../services";
+import { customErrorHandler, jwtService } from "../services";
 
 const auth = async (req, res, next) => {
   // Accessing the access token from headers
   let authHeader = req.headers.authorization;
   if (!authHeader) {
-    return next(CustomErrorHandler.unAuthorized("accessToken Required"));
+    return next(customErrorHandler.unAuthorized("accessToken Required"));
   }
   const authAccessToken = authHeader.split(" ")[1];
 
   //Verifying the access token
   try {
-    const { _id, role } = JwtService.verify(authAccessToken);
+    const { _id, role } = jwtService.verify(authAccessToken);
     const user = {
       _id,
       role,
     };
     req.user = user;
     next();
-  } catch (err) {
-    return next(CustomErrorHandler.unAuthorized(err.message));
+  } catch (error) {
+    return next(customErrorHandler.unAuthorized(error.message));
   }
 };
 
